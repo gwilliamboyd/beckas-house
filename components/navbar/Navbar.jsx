82cfcf7577a06088bb1328abe9316140a3f24c32
gwilliamboyd <gwillboyd@gmail.com>
2025-09-04 21:43:55 -0500

    Adds contact links to navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import styles from '../../styles/navbar.module.css'
 import CTAButton from '../CTAButton'
+import SocialIcon from '../SocialIcon'
+import NavlinkContact from './nav-links/NavlinkContact'
 import NavlinkMain from './nav-links/NavlinkMain'
 
 const Navbar = () => {
@@ -20,6 +22,34 @@ const Navbar = () => {
           buttonText={'Donate'}
           url={'#'}
         />
+      </div>
+      <div className={styles.contactContainer}>
+        <div className={styles.contactLeft}>
+          <NavlinkContact
+            styles={styles}
+            linkText={'214-952-8865'}
+            url={'tel:2149528865'}
+          />
+          <NavlinkContact
+            styles={styles}
+            linkText={'contact@beckashouse.com'}
+            url={'mailto:contact@beckashouse.com'}
+          />
+        </div>
+        <div className={styles.socials}>
+          <SocialIcon
+            styles={styles}
+            url={'https://www.instagram.com'}
+            fillColor={'#000000'}
+            site={'instagram'}
+          />
+          <SocialIcon
+            styles={styles}
+            url={'https://www.instagram.com'}
+            fillColor={'#000000'}
+            site={'instagram'}
+          />
+        </div>
       </div>
     </nav>
   )
--- components/navbar/nav-links/NavlinkContact.jsx
+++ components/navbar/nav-links/NavlinkContact.jsx
@@ -1,6 +1,13 @@
-const NavlinkContact = ({ linkText, url }) => {
+import { Open_Sans } from 'next/font/google'
+
+const open_sans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
+
+const NavlinkContact = ({ styles, linkText, url }) => {
   return (
-    <span>
+    <span className={`${open_sans.className} ${styles.navLinkContact}`}>
       <a href={url}>{linkText}</a>
     </span>
   )
