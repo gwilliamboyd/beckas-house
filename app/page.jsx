f2d0ee626187d43ddf8e8557495cdaceec8d8be8
gwilliamboyd <gwillboyd@gmail.com>
2025-09-06 19:37:20 -0500

    Adds hero image, page to jsx file, comments css files

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import styles from '@/styles/page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.pageMaster}>
+      {/* Hero section */}
+      <div className={styles.heroMaster}></div>
+    </main>
+  )
+}
