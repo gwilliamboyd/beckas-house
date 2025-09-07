51ec072d328f5eb9c72ccf3e8eab215d51b74ea7
gwilliamboyd <gwillboyd@gmail.com>
2025-09-06 19:20:10 -0500

    Adds logo and adjusts navbar padding

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styles from '../../styles/navbar.module.css'
 import CTAButton from '../CTAButton'
 import SocialIcon from '../SocialIcon'
@@ -7,48 +8,59 @@ import NavlinkMain from './nav-links/NavlinkMain'
 const Navbar = () => {
   return (
     <nav className={`${styles.master}`}>
-      <div className={styles.mainContainer}>
-        <NavlinkMain
-          styles={styles}
-          linkText={'About Us'}
-          url={'#about-us'}
-        />
-        <NavlinkMain
-          styles={styles}
-          linkText={'FAQ'}
-          url={'#faq'}
-        />
-        <CTAButton
-          buttonText={'Donate'}
-          url={'#'}
-        />
-      </div>
-      <div className={styles.contactContainer}>
-        <div className={styles.contactLeft}>
-          <NavlinkContact
-            styles={styles}
-            linkText={'214-952-8865'}
-            url={'tel:2149528865'}
-          />
-          <NavlinkContact
-            styles={styles}
-            linkText={'contact@beckashouse.com'}
-            url={'mailto:contact@beckashouse.com'}
+      <div className={styles.innerContainer}>
+        <div className={styles.logoContainer}>
+          <Image
+            className={styles.logoImage}
+            src={'/beckas-house-logo.png'}
+            width={260}
+            height={120}
+            alt="Becka's House logo"
           />
         </div>
-        <div className={styles.socials}>
-          <SocialIcon
+        <div className={styles.mainContainer}>
+          <NavlinkMain
             styles={styles}
-            url={'https://www.instagram.com'}
-            fillColor={'#000000'}
-            site={'instagram'}
+            linkText={'About Us'}
+            url={'#about-us'}
           />
-          <SocialIcon
+          <NavlinkMain
             styles={styles}
-            url={'https://www.instagram.com'}
-            fillColor={'#000000'}
-            site={'instagram'}
+            linkText={'FAQ'}
+            url={'#faq'}
           />
+          <CTAButton
+            buttonText={'Donate'}
+            url={'#'}
+          />
+        </div>
+        <div className={styles.contactContainer}>
+          <div className={styles.contactLeft}>
+            <NavlinkContact
+              styles={styles}
+              linkText={'214-952-8865'}
+              url={'tel:2149528865'}
+            />
+            <NavlinkContact
+              styles={styles}
+              linkText={'contact@beckashouse.com'}
+              url={'mailto:contact@beckashouse.com'}
+            />
+          </div>
+          <div className={styles.socials}>
+            <SocialIcon
+              styles={styles}
+              url={'https://www.instagram.com'}
+              fillColor={'#000000'}
+              site={'instagram'}
+            />
+            <SocialIcon
+              styles={styles}
+              url={'https://www.instagram.com'}
+              fillColor={'#000000'}
+              site={'instagram'}
+            />
+          </div>
         </div>
       </div>
     </nav>
