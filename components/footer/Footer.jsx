2f9162f3493776d6698ef9c5b024c8489d241dc5
gwilliamboyd <gwillboyd@gmail.com>
2025-09-16 22:02:02 -0500

    Scaffolds footer socket

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -17,59 +17,70 @@ const bodoni_moda = Bodoni_Moda({
 
 const Footer = () => {
   return (
-    <footer className={styles.footerMaster}>
-      <div className={styles.footerWrapper}>
-        <div className={`${open_sans.className} ${styles.contactWrapper}`}>
-          <Image
-            src={'/beckas-house-logo.png'}
-            width={241}
-            height={111}
-            alt="Becka's House Logo"
-          />
-          <a
-            className={styles.contactText}
-            href='mailto:contact@beckashouse.com'>
-            contact@beckashouse.com
-          </a>
-          <a
-            className={styles.contactText}
-            href='tel:214-952-8865'>
-            214-952-8865
-          </a>
-        </div>
-        <div className={`${bodoni_moda.className} ${styles.linksWrapper}`}>
-          <NavlinkMain
-            styles={styles}
-            url={'#about-us'}
-            linkText={'About'}
-          />
-          <NavlinkMain
-            styles={styles}
-            url={'#faq'}
-            linkText={'FAQ'}
-          />
-          <NavlinkMain
-            styles={styles}
-            url={'/gallery'}
-            linkText={'Gallery'}
-          />
-        </div>
-        <div className={styles.socialsWrapper}>
-          <SocialIcon
-            styles={styles}
-            url={'https://www.instagram.com'}
-            fillColor={'#000000'}
-            site={'instagram'}
-          />
-          <SocialIcon
-            styles={styles}
-            url={'https://www.instagram.com'}
-            fillColor={'#000000'}
-            site={'instagram'}
-          />
+    <footer>
+      <div className={styles.footerMaster}>
+        <div className={styles.footerWrapper}>
+          <div className={`${open_sans.className} ${styles.contactWrapper}`}>
+            <Image
+              src={'/beckas-house-logo.png'}
+              width={241}
+              height={111}
+              alt="Becka's House Logo"
+            />
+            <a
+              className={styles.contactText}
+              href='mailto:contact@beckashouse.com'>
+              contact@beckashouse.com
+            </a>
+            <a
+              className={styles.contactText}
+              href='tel:214-952-8865'>
+              214-952-8865
+            </a>
+          </div>
+          <div className={`${bodoni_moda.className} ${styles.linksWrapper}`}>
+            <NavlinkMain
+              styles={styles}
+              url={'#about-us'}
+              linkText={'About'}
+            />
+            <NavlinkMain
+              styles={styles}
+              url={'#faq'}
+              linkText={'FAQ'}
+            />
+            <NavlinkMain
+              styles={styles}
+              url={'/gallery'}
+              linkText={'Gallery'}
+            />
+          </div>
+          <div className={styles.socialsWrapper}>
+            <SocialIcon
+              styles={styles}
+              url={'https://www.instagram.com'}
+              fillColor={'#000000'}
+              site={'instagram'}
+            />
+            <SocialIcon
+              styles={styles}
+              url={'https://www.instagram.com'}
+              fillColor={'#000000'}
+              site={'instagram'}
+            />
+          </div>
         </div>
       </div>
-      <div className={styles.footerSocket}></div>
+      <div className={`${open_sans.className} ${styles.footerSocketMaster}`}>
+        <div className={styles.footerSocketWrapper}>
+          <p className={styles.footerSocketText}>
+            © 2025 Becka’s House LLC | All Rights Reserved
+          </p>
+          <p className={styles.footerSocketText}>
+            Site by Henry Winget and G William Boyd
+          </p>
+        </div>
+      </div>
     </footer>
   )
 }
