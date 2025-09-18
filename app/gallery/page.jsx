96127f88218b741ab15ac023713bb53f0276cf11
gwilliamboyd <gwillboyd@gmail.com>
2025-09-17 21:17:13 -0500

    Adds loading component and reduces renders in gallery

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -2,71 +2,38 @@ import styles from '@/styles/gallery.module.css'
 import fs from 'fs'
 // import Image from 'next/image'
 import path from 'path'
+// import { useMemo } from 'react'
+import Image from 'next/image'
 
-// auto-detect images in folder
-const getImageCount = () => {
+const page = () => {
+  // auto-detect images in folder
+  // set directory
   const imgDir = path.join(process.cwd(), 'public/gallery-images/')
+  // define files
   const files = fs.readdirSync(imgDir)
   const images = files.filter(file =>
     /\.(png|jpe?g|gif|webp|avif|svg)$/i.test(file)
   )
 
-  const imgArr = []
-
-  for (let i = 0; i < images.length; i++) {
-    imgArr.push(images[i])
-  }
-
-  return imgArr
-}
-
-// set masonry layout image heights
-// prettier-ignore
-const setMasonry = () => {
-  const imgSizes = [
-    '1em', '2em', '3em', '4em', '1.4em',
-    '1.8em', '2.6em', '2.2em', '3.4em', '3.2em',
-  ]
-  const images = document.querySelectorAll(`${styles.galleryImg}`)
-  for (let i = 0; i < images.length; i++) {
-    images[i].styles.inlineSize = imgSizes[i]
-  }
-}
-
-// setMasonry()
-
-const page = () => {
-  const imgSizes = [
-    '8em',
-    '7em',
-    '6em',
-    '5em',
-    '8.4em',
-    '7.8em',
-    '6.6em',
-    '5.2em',
-    '8.8em',
-    '7.2em',
-  ]
   return (
     <main className={styles.galleryMaster}>
       <div className={styles.galleryContainer}>
-        {getImageCount().map((img, key) => {
-          console.log(img)
-          // const imgHeight = setMasonry()
+        {images.map((img, key) => {
           return (
-            <img
+            <Image
               key={key}
               className={styles.galleryImg}
               src={`/gallery-images/${img}`}
+              // loading component
+              placeholder='blur'
+              blurDataURL='/placeholder.png'
+              // size still from CSS
+              width={800}
+              height={600}
+              unoptimized
+              // maintains masonry image sizes
+              style={{ width: '100%', height: 'auto' }}
               alt={img}
-              // width={'auto'}
-              // style={{
-              //   height: imgSizes[key % imgSizes.length],
-              //   objectFit: 'cover',
-              //   objectPosition: 'center',
-              // }}
-              // height={imgHeight}
             />
           )
         })}
